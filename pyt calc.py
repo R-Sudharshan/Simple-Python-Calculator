d9d4c16d7f5a6fdf699a320dcfb0d0b25b6f1ebf
R-Sudharshan <141005974+R-Sudharshan@users.noreply.github.com>
2023-08-02 00:43:55 +0530

    Update pyt calc.py
    Updated for Invalid Inputs

--- pyt calc.py
+++ pyt calc.py
@@ -5,12 +5,13 @@ print("Enter your values and then enter the operation for instant and accurate a
 a = int(input("First no.:"))
 b = int(input("Second no.:"))
 operation = input("1.add,2.subtract,3.Multiply,4.Divide\n")
-if(operation =="1" or "add"):
+if(operation == "add"):
    print(a+b)
-elif(operation == "2" or "subtract"):
+elif(operation == "subtract"):
      print(a-b)
-elif(operation == "3" or "multiply"):
+elif(operation == "multiply"):
      print(a*b)
-elif(operation == "4" or "divide"):
+elif(operation == "divide"):
      print(a/b)
-
+else:
+   print("Invalid Output")
